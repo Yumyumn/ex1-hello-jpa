dfe7100a7f6698baf38ebaa39215739dc79c3c15
jean202 <hb13pud731@gmail.com>
2022-04-11 18:47:07 +0900

    220411 Study

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
-        // 로딩 시점에 딱 하나만 만들어야 한다.
+        // 로딩 시점에 딱 하나만 만들어야 한다. (DB당 1개)
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         // 내부적으로 database connection을 물고 동작한다.
         EntityManager em = emf.createEntityManager();
@@ -19,6 +19,8 @@ public class JpaMain {
         try{
             /* // 삽입
             Member member = new Member();
+//            member.setId(1L);
+//            member.setName("HelloA");
             member.setId(2L);
             member.setName("HelloB");
             em.persist(member);
@@ -27,28 +29,32 @@ public class JpaMain {
             Member findMember = em.find(Member.class, 1L);
             System.out.println("findMember.id = " + findMember.getId());
             System.out.println("findMember.name = " + findMember.getName());
+            */
+            /*
             // 삭제
             em.remove(findMember);
              */
             /*// 수정
             Member findMember = em.find(Member.class, 1L);
+            // transaction을 커밋하기 직전에 JPA가 변화된 내용을 감지하고 update문을 날리고 tx을 커밋한다
             findMember.setName("HelloJPA");
              */
             // JPQL 사용
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
             // JPA는 절대 테이블을 대상으로 쿼리를 만들지 않는다. 멤버 객체를 대상으로 한 쿼리문이다.
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
             // 만약 페이징을 하고 싶다면
-            List<Member> resultP = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1) // 1번부터 10개 가져와! 라는 뜻
-                    .setMaxResults(10)
-                    .getResultList();
+//            List<Member> resultP = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1) // 1번부터 10개 가져와! 라는 뜻
+//                    .setMaxResults(10)
+//                    .getResultList();
             tx.commit();
         }catch (Exception e){
             tx.rollback();
         }finally {
+            // entity manager가 내부적으로 database connection을 물고 동작하기 때문에 이것을 닫아주는게 중요하다
             em.close();
         }
         emf.close();
